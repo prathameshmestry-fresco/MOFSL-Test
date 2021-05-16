7e8de36c083cc4af208d151feb4dda63ab20b809
Prathamesh Mestry <prathameshmestry1992@gmail.com>
2021-05-16 12:07:53 +0530

    remove commented lines

--- Motilal-Test/AppDelegate.swift
+++ Motilal-Test/AppDelegate.swift
@@ -15,7 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
-        // Override point for customization after application launch.
         return true
     }
     
